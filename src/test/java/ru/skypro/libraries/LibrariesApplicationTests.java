f38fce6ad409150b3f8f8025bd5096ee7b3f5214
marat <marat19955@gmail.com>
2023-05-02 07:20:21 +0300

    completed

---
+++ src/test/java/ru/skypro/libraries/LibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
